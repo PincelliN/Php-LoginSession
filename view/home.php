a36dfffedfb618aec27b77d444eca9bdc9b7562d
Nicolò Pincelli <pincelli.n@gmail.com>
2024-11-27 18:30:10 +0100

    feat: add page home

---
+++ view/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/home.css">
+    <title>Benvenuto</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Benvenuto
+            <!-- <?php echo $user->name; ?> -->
+        </h1>
+        <a class="button profile" href="">Modifica il profilo</a>
+        <a class="button logout" href="">Logout</a>
+    </div>
+
+</body>
+
+</html>
