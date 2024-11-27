a9e2afacc545d02dcfc703dbb230fe258ff20e71
Nicolò Pincelli <pincelli.n@gmail.com>
2024-11-27 16:14:13 +0100

    feat: Hashing password

--- controller/register.php
+++ controller/register.php
@@ -23,3 +23,19 @@ $query->execute();
 //fetch metodo che recupera il primo risultato della query eseguita
 //PDO::FETCH_OBJ specifica che il risultato sarà un oggetto che utilizzera i nomi delle colonne propietà del oggetto restituito  
 $user = $query->fetch(PDO::FETCH_OBJ);
+
+if ($user) {
+    //throw lancia un eccezione e gestisce l'errore
+    throw new Exception('L \' email inserit\à esiste gi\à');
+}
+if ($password != $passwordConfirm) {
+    throw new Exception('Le password non corrispondono');
+}
+
+//rendiamo robusto l'hash
+//generiamo un id unico (uniqid()) calcoliamo l'hash (md5) che generera una stringa di 32 caratteri e successivamente prenderemo una sottostringa che iniziera dal carattere in posizone 10 e avra una lenght di 10
+$salt = substr(md5(uniqid()), 10, 10);
+//sha1 genera un l' hash  di 40 caratteri al posto dei 32 di (md5)
+$token = sha1(uniqid());
+//PASSWORD_BCRYPT è un algoritmo di hashing  per la criptografia delle password che è volutamente lento e costoso computazionalmente 
+$password = password_hash(PEPPER . $password . $salt, PASSWORD_BCRYPT);
